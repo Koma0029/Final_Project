95f45e3b2b5dc9bae06db7505477a8839d89ffc4
Manisha <mani0076@algonquinlive.com>
2024-04-12 17:26:39 +0530

    Soccer match complete

--- app/src/main/java/com/example/final_project/movie/interfaces/ClickInterface.java
+++
@@ -1,8 +0,0 @@
-package com.example.final_project.movie.interfaces;
-
-
-import com.example.final_project.movie.model.ResponseData;
-
-public interface ClickInterface {
-    void onDetail(ResponseData movieSearchModel);
-}
--- app/src/main/java/com/example/final_project/ticket/AppConsts.java
+++
@@ -1,5 +0,0 @@
-package com.example.final_project.ticket;
-
-public class AppConsts {
-    String language = "language";
-}
